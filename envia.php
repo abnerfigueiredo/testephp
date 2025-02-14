9c6c06b75ad8403dcfd3457702856ef7e51aa080
abnerfigueiredo <abner.luiz23@hotmail.com>
2025-02-14 09:07:48 -0300

    Add files via upload

---
+++ envia.php
@@ -0,0 +1,21 @@
+<?php
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $mensagen = ($_POST['email']);
+
+    $para = "abner.luiz23@gmail.com;";
+    $assunto = "Coleta de dados - Cartão Saúde Fácil";
+    
+    $corpo = "Nome: ".$nome."\n"."E-mail:".$email."\n".
+
+    $cabeca = "From: abner.luiz23@hotmail.com"."\n"."Reply-to:".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-ail enviado com sucesso");
+}else{
+    echo("Houve um erro ao enviar o email");
+}
+
+
+?>
